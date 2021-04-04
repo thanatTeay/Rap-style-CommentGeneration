9ce1885eb0feceb27dccc77f902cd424acd0c16e
thanatTeay <79521432+thanatTeay@users.noreply.github.com>
2021-04-05 00:51:43 +0900

    final

---
+++ texttospeechAPG/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,490 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Speech.AudioFormat;
+using System.Threading;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string textForSpeak, textP1, textP2, textCheering, P1,P2, textAll;
+        public string checkNewAudio = "A";
+        public string oldAudio = "A", commentP1strong = "Now, P1 is stronger than P2." , commentP2strong = "Now, P2 is stronger than P1.";
+        string copyWords;
+        string midiAll;
+        string[] words, midi;
+        bool rap = true;
+        int cSpeech = 0, typeMidi = 0, typeMusic = 0, newStrong=0;
+        double cMidi = 0, cMidiFuture = 0;
+
+        public int typeRhythm = 0;
+        public bool checkBGM,p1Strong,p2Strong;
+        public int setTime = -999;
+        public double beat = 900;
+        public double P1Value = 1, P2Value = 0;
+        public int setTimeP = 2000;
+        string path1 = @"C:\Users\maili\Desktop\texttospeechAPG\textfile\";
+        string path2 = @"D:\FTGexp\F\";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void readMidi()
+        {
+            System.IO.File.WriteAllText(path1+"testComments.txt", "");
+            typeMidi = 0;
+            Random random = new Random();
+            int tyrandomNumber = random.Next(0, 3);
+            midi = null;
+            typeMidi = tyrandomNumber;
+            if (typeMidi == 0)
+            {
+                midiAll = System.IO.File.ReadAllText(path1+"How Many Mics testing(MIDI).txt");
+            }
+            else if (typeMidi == 1)
+            {
+                midiAll = System.IO.File.ReadAllText(path1+"Dove.txt");
+            }
+            else if (typeMidi == 2)
+            {
+                midiAll = System.IO.File.ReadAllText(path1+"Pras.txt");
+            }
+            else
+            {
+
+                midiAll = System.IO.File.ReadAllText(path1+"Lost Yourself(MIDI).txt");
+            }
+            for (int i = 0; i < midiAll.Length; i++)
+            {
+                midi = midiAll.Split(',', '\n', ' ');
+
+            }
+            //Console.WriteLine(midi.Length + " " + typeMidi);
+            //haveMidi();
+        }
+        private void rePlay()
+        {
+            //backingBeat();
+        }
+        private void backingBeat()
+        {
+            /* System.Media.SoundPlayer soundP = new System.Media.SoundPlayer();
+             Random random = new Random();
+             int tyrandomMusic = random.Next(0, 2);
+             typeMusic = tyrandomMusic;
+             //typeMusic = 1;
+             if (typeMusic == 0)
+             {
+                 soundP.SoundLocation = @"C:\Users\Thanat Jumneanbun\Documents\TextToSpeech1\backingBeat\FreeBeats.wav";
+
+             }
+             else if (typeMusic == 1)
+             {
+                 soundP.SoundLocation = @"C:\Users\Thanat Jumneanbun\Documents\TextToSpeech1\backingBeat\FreeBeats2.wav";
+
+             }*/
+
+
+            chooseBeat();
+            //soundP.Load();
+            //soundP.Play();
+
+            //Thread.Sleep(setTimeP);
+            //readWords();
+
+        }
+
+        private void chooseBeat()
+        {
+            if (typeMusic == 0)
+            {
+                if (typeMidi == 0)
+                {
+                    beat = 900;
+                    setTimeP = 7000;
+                }
+                else if (typeMidi == 1)
+                {
+                    beat = 900;
+                    setTimeP = 7500;
+                }
+                else if (typeMidi == 2)
+                {
+                    beat = 900;
+                    setTimeP = 7000;
+                }
+                else
+                {
+                    beat = 900;
+                    setTimeP = 7800;
+                }
+            }
+            else if (typeMusic == 1)
+            {
+                if (typeMidi == 0)
+                {
+                    beat = 900;
+                    setTimeP = 9000;
+                }
+                else if (typeMidi == 1)
+                {
+                    beat = 900;
+                    setTimeP = 9000;
+                }
+                else if (typeMidi == 2)
+                {
+                    beat = 900;
+                    setTimeP = 9800;
+                }
+                else
+                {
+                    beat = 900;
+                    setTimeP = 9000;
+                }
+            }
+        }
+
+        private void readWords()
+        {
+            
+            try
+            {
+                DateTime fileCreatedDate1 = File.GetLastWriteTime(path2+"countP1.txt");
+                checkNewAudio = fileCreatedDate1.ToString("HH:mm:ss");
+                //textForSpeak = System.IO.File.ReadAllText(@"C:\Users\Thanat Jumneanbun\Documents\texttospeechAPG\textfile\testComments.txt");
+                textP1 = System.IO.File.ReadAllText(path1+"P1Comment.txt");
+                textP2 = System.IO.File.ReadAllText(path1+"P2Comment.txt");
+                textCheering = System.IO.File.ReadAllText(path1+"Cheering.txt");
+                P1 = System.IO.File.ReadLines(path2+"P1Hp.txt").First();
+                P2 = System.IO.File.ReadLines(path2+ "P2Hp.txt").First();
+                Double.TryParse(P1, out P1Value);
+                Double.TryParse(P2, out P2Value);
+            }
+            catch
+            {
+                DateTime fileCreatedDate1 = File.GetLastWriteTime(path2+"countP1.txt");
+                checkNewAudio = fileCreatedDate1.ToString("HH:mm:ss");
+                textP1 = System.IO.File.ReadAllText(path1+"P1Comment.txt");
+                textP2 = System.IO.File.ReadAllText(path1+"P2Comment.txt");
+                textCheering = System.IO.File.ReadAllText(path1+"Cheering.txt");
+                P1 = System.IO.File.ReadLines(path2 + "P1Hp.txt").First();
+                P2 = System.IO.File.ReadLines(path2 + "P2Hp.txt").First();
+                Double.TryParse(P1, out P1Value);
+                Double.TryParse(P2, out P2Value);
+            }
+
+            if (oldAudio == checkNewAudio)
+            {
+
+                if (P1Value > P2Value)
+                {
+                    if (p1Strong == false)
+                    {
+                        p1Strong = true;
+                        p2Strong = false;
+                        textAll = textP1;
+                        textForSpeak = textAll;
+                        textForSpeak = commentP1strong;
+                        System.IO.File.WriteAllText(path1+"testComments.txt", textForSpeak);
+
+                    }
+                    else
+                    {
+                        textAll = textP1;
+                        textForSpeak = textAll;
+                        System.IO.File.WriteAllText(path1+"testComments.txt", commentP1strong);
+                    }
+                }
+                else if(P1Value == P2Value)
+                {
+                    System.IO.File.WriteAllText(path1+"testComments.txt", " ");
+                    Random random = new Random();
+                    int tyrandomNumber = random.Next(0, 2);
+                    if(tyrandomNumber ==0)
+                    {
+                        textAll = textP1;
+                        textForSpeak = textAll;
+                    }
+                    else
+                    {
+                        textAll = textP2;
+                        textForSpeak = textAll;
+                    }
+                    
+
+
+                }
+                else if (P1Value < P2Value)
+                {
+                    
+                    if (p2Strong == false)
+                    {
+                        p1Strong = false;
+                        p2Strong = true;
+                        textAll = textP2;
+                        textForSpeak = textAll;
+                        textForSpeak = commentP2strong;
+                        System.IO.File.WriteAllText(path1+"testComments.txt", textForSpeak);
+                    }
+                    else
+                    {
+                        textAll = textP2;
+                        textForSpeak = textAll;
+                        System.IO.File.WriteAllText(path1+"testComments.txt", commentP2strong);
+                    }
+
+                }
+                oldAudio = checkNewAudio;
+            }
+            else
+            {
+                textAll = textCheering;
+                textForSpeak = textAll;
+                oldAudio = checkNewAudio;
+            }
+            /*Console.WriteLine("check text all: " + textForSpeak);
+            Console.WriteLine("check text all: " + textForSpeak.Length);
+            Console.WriteLine("check P1 text all: " + textP1);
+            Console.WriteLine("check P2 text all: " + textP2);*/
+            Console.WriteLine("check P1 Value: " + P1Value);
+            Console.WriteLine("check P2 Value: " + P2Value);
+
+             if(rap)
+            {
+                if (textAll != copyWords)
+                {
+                    System.IO.File.WriteAllText(path1+"subComments.txt", textForSpeak);
+                    for (int i = 0; i < textForSpeak.Length; i++)
+                    {
+                        words = textForSpeak.Split(' ', '\n');
+
+                    }
+
+                    //Console.WriteLine("words= " + words);
+                    //Console.WriteLine("textForSpeak.Length= " + textForSpeak.Length);
+                    //speechWord();
+                }
+                else
+                {
+                    introBeforeCommnet();
+                }
+            }
+            
+
+        }
+
+
+        private void speechWord()
+        {
+            //rhythm();
+            for (int i = 0; i < midi.Length; i++)
+            {
+                double.TryParse(midi[i], out cMidi);
+                if (i < midi.Length - 1)
+                {
+                    double.TryParse(midi[i + 1], out cMidiFuture);
+                }
+
+                if (cMidiFuture < 0)
+                {
+                    cMidi = cMidi + (cMidiFuture * -1);
+                    i++;
+                }
+                //Console.WriteLine("I= "+i+" cMidi= "+cMidi);
+
+                setTime = Convert.ToInt32(beat * cMidi);
+                Console.WriteLine(setTime+" = "+beat+" * "+ cMidi);
+                addSpeed();
+
+            }
+            //after read all rythm done
+            cSpeech = 0;
+
+            // introBeforeCommnet();
+            //Speech.synth.Dispose();
+            readMidi();
+            readWords();
+            speechWord();
+
+        }
+
+        private void addSpeed()
+        {
+            if (setTime >= (beat * 1)) //1.0
+            {
+                Speech.rateSpeed = 2;
+            }
+            else if (setTime >= (beat * 0.5)) //0.50
+            {
+                Speech.rateSpeed = 4;
+            }
+            else if (setTime >= (beat * 0.25))
+            {
+                Speech.rateSpeed = 5;
+            }
+            else
+            {
+                Speech.rateSpeed = 7;
+            }
+            checkWord();
+        }
+
+        private void checkWord()
+        {
+            if (textForSpeak.Length == 0)
+            {
+                introBeforeCommnet();
+            }
+            else
+            {
+                if (cSpeech != words.Length)
+                {
+
+                    //Console.WriteLine(cSpeech);
+
+                    //Speech.setLanguage2();
+                    //Speech.synth.Dispose();
+                    /*if (words[cSpeech].Length <= 5)
+                    {
+                        Speech.synth.Dispose();
+                    }
+                    else
+                    {
+                        setTime = 900;
+                    }*/
+                    
+                    /*if (words[cSpeech].Length < 5 )
+                    {
+                        //Speech.synth.Dispose();
+                        Console.WriteLine("Disposed: " + cSpeech);
+                    }*/
+                    if (words[cSpeech].Length >= 5)
+                    {
+                        setTime = 900;
+                    }
+
+                    // Console.WriteLine(words[cSpeech]);
+                    // Console.WriteLine("words length: "+ words[cSpeech].Length);
+                    Speech.speak_cont(words[cSpeech]);
+                    // Console.WriteLine("Settime: "+setTime);
+                    Thread.Sleep(setTime);
+                    
+                   
+
+                    cSpeech++;
+
+                }
+                else
+                {
+                    
+                    copyWords = textAll;
+                    cSpeech = 0;
+                    textForSpeak = null;
+                    words = null;
+                    //Speech.synth.Dispose();
+                    //Speech.setLanguage(Speech.getVoiceList()[1]);
+                    readWords();
+                }
+
+            }
+
+        }
+
+        private void introBeforeCommnet()
+        {
+            textForSpeak = "yo";
+            for (int i = 0; i < textForSpeak.Length; i++)
+            {
+                words = textForSpeak.Split(' ', '\n');
+            }
+
+            //rhythm();
+            for (int i = 0; i < words.Length; i++)
+            {
+                cSpeech = 0;
+                System.IO.File.WriteAllText(path1+"subComments.txt", "yo");
+                Random random = new Random();
+                int preCheck = random.Next(0, 3);
+                if (preCheck == 0)
+                {
+                    cMidi = 0.25;
+                }
+                else if (preCheck == 1)
+                {
+                    cMidi = 0.5;
+                }
+                else
+                {
+                    cMidi = 0.75;
+                }
+                //Console.WriteLine("I= " + i + " cMidi= " + cMidi);
+                setTime = Convert.ToInt32(beat * cMidi);
+                addSpeed();
+
+            }
+            //after read all rythm done
+            cSpeech = 0;
+            Thread.Sleep(700);
+        }
+
+        private void normalTTS()
+        {
+            rap = false;
+            readWords();
+            //textForSpeak = System.IO.File.ReadAllText(path1+"P1Comment.txt");
+
+            Speech.rateSpeed = 0;
+            Speech.speak(textForSpeak);
+            copyWords = textForSpeak;
+            normalTTS();
+
+
+
+        }
+        private void rapTTS()
+        {
+            rap = true;
+            System.IO.File.WriteAllText(path1+"subComments.txt", " ");
+            System.IO.File.WriteAllText(path1+"testComments.txt", " ");
+            System.IO.File.WriteAllText(path1+"P1Comment.txt", " ");
+            System.IO.File.WriteAllText(path1+"P2Comment.txt", " ");
+            DateTime fileCreatedDate1 = File.GetLastWriteTime(path2+"countP1.txt");
+            checkNewAudio = fileCreatedDate1.ToString("HH:mm:ss");
+            oldAudio = checkNewAudio;
+            readMidi();
+            //backingBeat();
+            Thread.Sleep(setTimeP);
+            introBeforeCommnet();
+            readWords();
+            Console.WriteLine("readword in button");
+            speechWord();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Speech.setLanguage(Speech.getVoiceList()[1]);
+            rapTTS();
+         // normalTTS();
+        }
+
+        public void Exit()
+        {
+            System.IO.File.WriteAllText(path1+"subComments.txt", " ");
+            System.IO.File.WriteAllText(path1+"testComments.txt", " ");
+
+
+        }
+    }
+}
---
+++ texttospeechAPG/WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Speech.AudioFormat;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    class Speech
+    {
+        public static int vol = 100;
+        public static int rateSpeed = 0;
+        public static SpeechSynthesizer synth = new SpeechSynthesizer { Volume = vol, Rate = rateSpeed };
+        public static int voiceGender = 1;
+        static Boolean isPause = false;
+
+        static List<SpeechSynthesizer> list_sync = new List<SpeechSynthesizer>();
+
+
+        public static void initialize()
+        {
+            SpeechSynthesizer s = new SpeechSynthesizer { Volume = vol, Rate = rateSpeed };
+
+            list_sync.Add(s);
+            if (list_sync.Count > 5) { list_sync.First().Dispose(); list_sync.RemoveAt(0); }
+
+            synth = s;
+
+
+            // synth = new SpeechSynthesizer { Volume = vol, Rate = rateSpeed };
+            //setVoice(voiceGender);
+            try
+            {
+                if (lang != "")
+                {
+
+                    synth.SelectVoice(lang);
+                    // Console.WriteLine("words= " + lang);
+                }
+            }
+            catch
+            {
+                //initialize();
+            }
+            isPause = false;
+
+        }
+
+
+        //public static int i_s = 0;
+        //public static SpeechSynthesizer[] sync_arr = new SpeechSynthesizer[] {
+        //    new SpeechSynthesizer { Volume = vol, Rate = rateSpeed }, new SpeechSynthesizer { Volume = vol, Rate = rateSpeed }, new SpeechSynthesizer { Volume = vol, Rate = rateSpeed } };
+
+
+        //public static void initialize()
+        //{
+
+        //    sync_arr[i_s].Dispose();
+        //    sync_arr[i_s] = new SpeechSynthesizer { Volume = vol, Rate = rateSpeed };
+
+
+        //    i_s++; if (i_s > 2) { i_s = 0; }
+        //    synth = sync_arr[i_s];
+
+
+        //    synth = new SpeechSynthesizer { Volume = vol, Rate = rateSpeed };
+
+
+
+
+        //    synth = new SpeechSynthesizer { Volume = vol, Rate = rateSpeed };
+        //    setVoice(voiceGender);
+        //    try
+        //    {
+        //        if (lang != "")
+        //        {
+
+        //            synth.SelectVoice(lang);
+        //            Console.WriteLine("words= " + lang);
+        //        }
+        //    }
+        //    catch
+        //    {
+        //        initialize();
+        //    }
+
+
+
+        //    isPause = false;
+
+
+        //}
+
+
+
+        public static void exportFile()
+        {
+            //synth.SetOutputToWaveFile(@"C:\Users\Thanat Jumneanbun\Desktop\TextToSpeech\Wavfile\Sample.wav");
+
+        }
+
+        public static void speak(string s)
+        {
+            initialize();
+            synth.Speak(s);
+        }
+
+        public static void speak_cont(string s)
+        {
+            initialize();
+            //synth.Rate = rateSpeed;
+            //setVoice(voiceGender);
+            synth.SpeakAsync(s);
+            
+
+        }
+
+
+        public static void stop()
+        {
+            synth.SpeakAsyncCancelAll();
+        }
+
+        public static void pause_resume()
+        {
+            if (isPause) { synth.Resume(); }
+            else { synth.Pause(); }
+            isPause = !isPause;
+        }
+
+        public static void setVol(int i)
+        {
+            vol = i;//cant change during play
+        }
+
+        public static void setVoice(int i)
+        {
+            if (i == 1) { synth.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Teen); }
+            else if (i == 0) { synth.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Adult); }
+            else { synth.SelectVoiceByHints(VoiceGender.Neutral); }
+        }
+
+
+        //=========================================================================
+        public static string err_log = "";
+
+        //https://msdn.microsoft.com/en-us/library/ms586869(v=vs.110).aspx
+        public static List<string> getVoiceList()
+        {
+            List<string> list_s = new List<string>();
+            try
+            {
+                using (SpeechSynthesizer synth = new SpeechSynthesizer())
+                {
+                    foreach (InstalledVoice voice in synth.GetInstalledVoices())
+                    {
+                        VoiceInfo info = voice.VoiceInfo;
+                        string AudioFormats = "";
+                        foreach (SpeechAudioFormatInfo fmt in info.SupportedAudioFormats)
+                        {
+                            AudioFormats += String.Format("{0}\n",
+                            fmt.EncodingFormat.ToString());
+                        }
+                        list_s.Add(info.Name);
+                    }
+                }
+            }
+            catch (Exception ex) { err_log = ex.ToString(); }
+            return list_s;
+        }
+
+        public static List<string> getVoiceDetails()
+        {
+            List<string> list_s = new List<string>();
+            try
+            {
+                using (SpeechSynthesizer synth = new SpeechSynthesizer())
+                {
+                    // Output information about all of the installed voices. 
+                    list_s.Add("Installed voices -");
+                    foreach (InstalledVoice voice in synth.GetInstalledVoices())
+                    {
+                        VoiceInfo info = voice.VoiceInfo;
+                        string AudioFormats = "";
+                        foreach (SpeechAudioFormatInfo fmt in info.SupportedAudioFormats)
+                        {
+                            AudioFormats += String.Format("{0}\n",
+                            fmt.EncodingFormat.ToString());
+                        }
+                        list_s.Add(" Name:          " + info.Name);
+                        list_s.Add(" Culture:       " + info.Culture);
+                        list_s.Add(" Age:           " + info.Age);
+                        list_s.Add(" Gender:        " + info.Gender);
+                        list_s.Add(" Description:   " + info.Description);
+                        list_s.Add(" ID:            " + info.Id);
+                        list_s.Add(" Enabled:       " + voice.Enabled);
+                        if (info.SupportedAudioFormats.Count != 0)
+                        {
+                            list_s.Add(" Audio formats: " + AudioFormats);
+                        }
+                        else
+                        {
+                            list_s.Add(" No supported audio formats found");
+                        }
+
+                        string AdditionalInfo = "";
+                        foreach (string key in info.AdditionalInfo.Keys)
+                        {
+                            AdditionalInfo += String.Format("  {0}: {1}\n", key, info.AdditionalInfo[key]);
+                        }
+
+                        list_s.Add(" Additional Info - " + AdditionalInfo);
+                        list_s.Add("");
+                    }
+                }
+            }
+            catch { }
+            return list_s;
+        }
+
+        public static string lang = "";
+        public static void setLanguage(string l)
+        {
+            lang = l;
+            synth.SelectVoice(lang);
+        }
+
+        public static void setLanguage2()
+        {
+            var syn = new System.Speech.Synthesis.SpeechSynthesizer();
+            syn.SelectVoice("Microsoft Server Speech Text to Speech Voice (ja-JP, Haruka)");
+            syn.Speak("こんにちは");
+        }
+
+    }
+}
